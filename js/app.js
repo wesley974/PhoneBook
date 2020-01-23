5955f7323d681932d7054389019697da7bf0248c
Wesley <milo974@gmail.com>
2020-01-23 09:53:31 +0400

    Layout for phone numbers

--- js/app.js
+++ js/app.js
@@ -13,14 +13,26 @@ function filterUser(letters) {
         .then(data => tableAdd(data.filter(item => item.Nom.toLowerCase().includes(letters))));
 }
 
+function layoutPhone(n){
+  if (n.toString().length === 9) {
+    const A = n.toString().slice(0,3);
+    const B = n.toString().slice(3,5);
+    const C = n.toString().slice(5,7);
+    const D = n.toString().slice(7,9);
+    return "0" + A + " " + B + " " + C + " " + D;
+  } else {
+    return n;
+  }
+}
+
 function tableAdd(items) {
 	tbody = document.querySelector("tbody");
 	items.forEach(item => {
         tbody.insertAdjacentHTML("afterbegin", `
           <tr>
             <td>${item.Nom}</td>
-            <td>${item.Telephone}</td>
-            <td>${item.Mobile}</td>
+            <td>${layoutPhone(item.Telephone)}</td>
+            <td>${layoutPhone(item.Mobile)}</td>
             <td>${item.Email}</td>
           </tr>
         `);
