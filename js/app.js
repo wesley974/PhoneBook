f06737237d3957af887f029cc1b467e2321ded39
Wesley <milo974@gmail.com>
2020-01-23 10:18:58 +0400

    sort alphabetically the Name column

--- js/app.js
+++ js/app.js
@@ -25,9 +25,26 @@ function layoutPhone(n){
   }
 }
 
+function sortAlphabetically(property) {
+    let sortOrder = 1;
+
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+
+    return function (a,b) {
+        if(sortOrder == -1){
+            return b[property].localeCompare(a[property]);
+        }else{
+            return a[property].localeCompare(b[property]);
+        }        
+    }
+}
+
 function tableAdd(items) {
 	tbody = document.querySelector("tbody");
-	items.forEach(item => {
+	items.sort(sortAlphabetically("-Name")).forEach(item => {
         tbody.insertAdjacentHTML("afterbegin", `
           <tr>
             <td>${item.Nom}</td>
